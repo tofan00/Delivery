288db96b64a9e22625da957a6d7b0020bd8e3726
Maroshop <homworkhhh25@gmail.com>
2026-03-14 14:39:09 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,29 @@
+// firebase-config.js
+
+const firebaseConfig1885 = {
+  apiKey: "AIzaSyCnLAY7zQyBy7gUuL9wszt9aEhiJgvRmxI",
+  authDomain: "shop-d52dc.firebaseapp.com",
+  databaseURL: "https://shop-d52dc-default-rtdb.firebaseio.com",
+  projectId: "shop-d52dc",
+  storageBucket: "shop-d52dc.appspot.com",
+  messagingSenderId: "97580537866",
+  appId: "1:97580537866:web:abc46e5a2f527b6300a7f3",
+  measurementId: "G-956RQMBP42"
+};
+
+const FIREBASE_PATHS_1885 = {
+  settingsStore: "settings1885/store1885",
+  settingsBanners: "settings1885/banners1885",
+  settingsPaymentMethods: "settings1885/paymentMethods1885",
+  categories: "categories1885",
+  products: "products1885",
+  users: "users1885",
+  orders: "orders1885"
+};
+
+const STORAGE_KEYS_1885 = {
+  cart: "tofan_cart_1885",
+  wishlist: "tofan_wishlist_1885",
+  checkoutSelection: "tofan_checkout_selection_1885",
+  selectedCategory: "tofan_selected_category_1885"
+};
